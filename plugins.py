8ce408d928a613049e0d7d646a053d88c06e20e6
Curtis Maloney <curtis@tinbrain.net>
2019-05-09 06:26:07 +1000

    Replace TagCloud with TagIndex

--- plugins.py
+++ plugins.py
@@ -1,6 +1,7 @@
 import typing
 from collections import Counter
 from operator import attrgetter
+from pathlib import Path
 
 from gilbert.content import Content, Templated
 from gilbert.query import Query
@@ -27,14 +28,34 @@ class Menu(Templated, Content):
         )
 
 
-class TagCloud(Content):
+class TagIndex(Content):
     exclude_tags : typing.Collection[str] = set()
+    template = 'tag_index.html'
 
     filter_by : dict = {}
 
     @oneshot
+    def pages(self):
+        return self.site.pages.matching(self.filter_by)
+
+    @oneshot
     def tag_counts(self):
         tags = Counter()
-        for obj in self.site.pages.matching(self.filter_by):
+        for obj in self.pages:
             tags.update(tag for tag in obj.tags if tag not in self.exclude_tags)
         return tags
+
+    def render(self):
+        target_path = self.site.dest_dir / Path(self.name).with_suffix('')
+        target_path.mkdir(parents=True, exist_ok=True)
+
+        template = self.site.templates[self.template]
+        ctx = self.site.get_context(self)
+
+        for tag in self.tag_counts:
+            target = target_path / f'{tag}.html'
+            with ctx.push({
+                'tag': tag,
+                'pages': [page for page in self.pages if tag in page.tags]
+            }):
+                target.write_text( template.render(ctx) )
